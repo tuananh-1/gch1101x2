79134a226c460600a8fab6ac79b54b7a2a5b9f76
tuananh-1 <anhldtgdh210341@fpt.edu.vn>
2023-05-31 17:16:59 +0700

    lab8

--- lab06/models/SubjectModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-var SubjectSchema = new mongoose.Schema({
-   name: String,
-   code : Number
-})
-
-var SubjectModel = mongoose.model('subject', SubjectSchema, 'subject');
-module.exports = SubjectModel;
