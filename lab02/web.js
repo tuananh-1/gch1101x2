2ff127cbc6c79122a5e1be445d58bb3fe28be169
tuananh-1 <anhldtgdh210341@fpt.edu.vn>
2023-05-31 17:07:31 +0700

    deploy

--- lab02/web.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-//const port = 3000
-const server = http.createServer((req, res) => {
-   //res.statusCode = 200  //HTTP-OK
-   //res.setHeader("Content-Type","text/html")
-   res.end("<h1>Welcome to Greenwich Vietnam !</h1>")
-})
-server.listen(3000)
